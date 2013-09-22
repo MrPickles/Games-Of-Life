330f48cf973c92b4f1f5b329ac7194d7a2e9cb43
MrPickles <liu.andrew.x@gmail.com>
2013-09-22 07:09:17 -0400

    Revert "Revert "Updated version of console part of Conway's Game of Life""
    This reverts commit b751c30cef642e946af54e917b12131890e5b8cf.

---
+++ Conway's Game of Life/src/console/Organism.java
@@ -0,0 +1,69 @@
+package console;
+
+public class Organism {
+
+    /**
+     * The necessary number of neighboring organisms such that an empty space
+     * becomes occupied by a new organism. This value should be 0 to 8
+     * inclusive.
+     */
+    public static int numToBeBorn = 3;
+    /**
+     * Boolean array listing the accepted values for number of neighboring
+     * organisms such that the central organism survives. The index of the array
+     * represents the number of organisms neighboring the central organism.
+     */
+    public static boolean[] numsToSurvive = { false, false, true, true, false,
+	    false, false, false, false };
+
+    public Organism() {
+    }
+
+    /**
+     * Checks if an empty space has the correct number of neighboring organisms
+     * such that an organism should be born in said empty space.
+     */
+    public static boolean shouldBeBorn(int row, int col, int rowSize,
+	    int colSize, Organism[][] gridOfOrganisms) {
+	int sum = 0;
+	for (int i = -1; i <= 1; i++) {
+	    for (int j = -1; j <= 1; j++) {
+		if (i != 0 || j != 0) {
+		    if ((isInBounds(row + i, rowSize))
+			    && (isInBounds(col + j, colSize))
+			    && (gridOfOrganisms[row + i][col + j] != null)) {
+			sum++;
+		    }
+		}
+	    }
+	}
+	return sum == numToBeBorn;
+    }
+
+    /**
+     * Checks if an occupied space has the correct range of neighboring
+     * organisms such that the organism in said occupied space will remain in
+     * its space.
+     */
+    public static boolean shouldSurvive(int row, int col, int rowSize,
+	    int colSize, Organism[][] gridOfOrganisms) {
+	int sum = 0;
+	for (int i = -1; i <= 1; i++) {
+	    for (int j = -1; j <= 1; j++) {
+		if (i != 0 || j != 0) {
+		    if ((isInBounds(row + i, rowSize))
+			    && (isInBounds(col + j, colSize))
+			    && (gridOfOrganisms[row + i][col + j] != null)) {
+			sum++;
+		    }
+		}
+	    }
+	}
+	return numsToSurvive[sum];
+    }
+
+    public static boolean isInBounds(int coordinate, int size) {
+	return coordinate >= 0 && coordinate < size;
+    }
+
+}
---
+++ Conway's Game of Life/src/console/Tester.java
@@ -0,0 +1,39 @@
+package console;
+
+import java.util.Scanner;
+
+public class Tester {
+
+    public static void showGrid(Organism[][] myGrid) {
+	String s = "";
+	for (int i = 0; i < myGrid.length; i++) {
+	    for (int j = 0; j < myGrid[0].length; j++) {
+		if (myGrid[i][j] == null) {
+		    s += "0";
+		} else {
+		    s += "x";
+		}
+	    }
+	    s += "\n";
+	}
+	System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+	ConsoleGrid c = new ConsoleGrid(5, 6);
+	Organism[][] myGrid = c.getGrid();
+	myGrid[2][1] = new Organism();
+	myGrid[2][2] = new Organism();
+	myGrid[2][3] = new Organism();
+	c.setGrid(myGrid);
+
+	while (true) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Press Enter to continue: ");
+	    String s = sc.nextLine();
+	    showGrid(myGrid);
+	    c.act();
+	}
+    }
+
+}
